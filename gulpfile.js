073c2fbc0f687e8c25823bdf6b00003ec0c54644
Kriz <kruzhanivska.olena212@gmail.com>
2023-08-30 23:35:42 +0300

    Add functionality for include, images and fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,64 @@
 const {src, dest, watch, parallel, series} = require('gulp');
 
-const scss        = require('gulp-sass')(require('sass'));
-const concat      = require('gulp-concat');
-const uglify      = require('gulp-uglify-es').default;
-const browserSync = require('browser-sync').create();
+const scss         = require('gulp-sass')(require('sass'));
+const concat       = require('gulp-concat');
+const uglify       = require('gulp-uglify-es').default;
+const browserSync  = require('browser-sync').create();
+const clean        = require('gulp-clean');
 const autoprefixer = require('gulp-autoprefixer');
-const clean = require('gulp-clean');
+const avif         = require('gulp-avif');
+const webp         = require('gulp-webp');
+const imagemin     = require('gulp-imagemin');
+const newer        = require('gulp-newer');
+const fonter       = require('gulp-fonter');
+const ttf2woff2    = require('gulp-ttf2woff2');
+const svgSprite    = require('gulp-svg-sprite');
+const include      = require('gulp-include');
 
+function pages() {
+    return src('app/pages/*.html')
+    .pipe(include({
+        includePaths: 'app/components'
+    }))
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
+
+function fonts() {
+    return src('app/fonts/src/*.*')
+    .pipe(fonter({
+        formats: ['woff', 'ttf']
+    }))
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'))
+}
+
+function images() {
+    return src(['app/images/src/*.*', '!app/images/src/*.svg'])
+    .pipe(newer('app/images'))
+    .pipe(avif({ quality : 50 }))
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images'))
+    .pipe(webp())
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images'))
+    .pipe(imagemin())
+    .pipe(dest('app/images'))
+}
+
+function sprite() {
+    return src('app/images/*.svg')
+    .pipe(svgSprite({
+        mode: {
+            stack: {
+                sprite: '../sprite.svg',
+                example: true
+            }
+        }
+    }))
+    .pipe(dest('app/images'))
+}
 
 function scripts() {
     return src(['app/js/main.js'])
@@ -26,17 +78,16 @@ function styles() {
 }
 
 function watching() {
-    watch(['app/scss/style.scss'], styles)
-    watch(['app/js/main.js'], scripts)
-    watch(['app/**/*.html']).on('change', browserSync.reload)
-}
-
-function browsersync() {
     browserSync.init({
         server: {
-            baseDir: "app/"
+           baseDir: "app/"
         }
     });
+   watch(['app/scss/style.scss'], styles)
+    watch(['app/images/src'], images)
+    watch(['app/js/main.js'], scripts)
+    watch(['app/components/*', 'app/pages/*'], pages)
+    watch(['app/**/*.html']).on('change', browserSync.reload)
 }
 
 function cleanDist() {
@@ -47,18 +98,27 @@ function cleanDist() {
 function building() {
     return src([
         'app/css/style.min.css',
+        'app/images/*.*',
+        '!app/images/*.svg',
+        'app/images/sprite.svg',
+        'app/fonts/*.*',
         'app/js/main.min.js',
-        'app/**/*.html'
+        'app/*.html'
     ], {base : 'app'})
     .pipe(dest('dist'))
 }
 
-exports.styles = styles;
-exports.scripts = scripts;
+exports.styles   = styles;
+exports.images   = images;
+exports.fonts    = fonts;
+exports.pages    = pages;
+exports.sprite   = sprite;
+exports.scripts  = scripts;
 exports.watching = watching;
-exports.browsersync = browsersync;
+exports.building = building;
 
-exports.build = series(cleanDist, building);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.build    = series(cleanDist, building);
+exports.default  = parallel(styles, images, scripts, pages, watching);
+
 
 
